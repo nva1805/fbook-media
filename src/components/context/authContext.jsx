cf3349658742e08cd3f7c2fef85db19a9b978caf
nva1805 <nvananh1805@gmail.com>
2023-01-11 17:57:35 +0700

    add context cpn with function usecontext hook

---
+++ src/components/context/authContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const login = () => {
+    //TO DO
+    setCurrentUser({
+      id: 1,
+      name: "Nguyen Van Anh",
+      profilePic:
+        "https://i.pinimg.com/564x/ad/6d/e4/ad6de4abac3849f24980d901c657054e.jpg",
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
